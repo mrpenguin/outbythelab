a7d0bb51f49245c25412b4e0a9796a1b812ca4fd
THETACOSHELL\sadce <Sadcebu2003@yahoo.com>
2025-02-16 15:10:32 -0600

    Adding latest page. Changed navigation so that it scrolls the user back to the top after moving to a new page. Changed the backgrounds so that they will properly place themselves after a navigation.

---
+++ chunk-U5UFBGFB.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-PALOJITQ.js";import"./chunk-XZTTSED6.js";export{a as ComicDisplayComponent};
--- chunk-VF6C2GT4.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-HXD2FIS4.js";import"./chunk-3DLS4MAF.js";export{a as ComicDisplayComponent};
