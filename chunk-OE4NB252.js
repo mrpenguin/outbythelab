449d2025c0485a614dcc15325bb1cb52356f68d9
DESKTOP-AHA4MUL\sadce <sadcebu2003@yahoo.com>
2025-05-17 20:09:29 -0500

    Updating page list to display better for small screens and animate while appearing.

---
+++ chunk-OE4NB252.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-ZEYH3MGD.js";import"./chunk-BUSUMQ7A.js";export{a as ComicDisplayComponent};
--- chunk-QJREFTGL.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-3V3LOOM6.js";import"./chunk-RZJYASNV.js";export{a as ComicDisplayComponent};
